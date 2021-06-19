066c1854fa26890ad9be1eb0b6c7f2607b87fec5
Manhui Kudan <manhui0527@mail.ru>
2021-06-19 14:57:40 -0400

    second commit

---
+++ resources/js/app.js
@@ -0,0 +1,56 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+import axios from 'axios';
+import Vue from 'vue';
+import Datepicker from 'vuejs-datepicker';
+// require('./bootstrap');
+
+window.Vue = require('vue');
+window.axios = axios;
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+// const files = require.context('./', true, /\.vue$/i);
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+
+Vue.component('task-list', require('./components/Task-list.vue'));
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+Vue.mixin({
+    created: function () {
+        var myOption = this.$options.myOption
+        if (myOption) {
+            // console.log(myOption)
+            alert(myOption);
+        }
+    }
+})
+
+const app = new Vue({
+    el: '#app',
+    myOption: 'hello',
+});
+
+const date_picker = new Vue({
+    el: '#date_picker',
+
+    components: {
+        Datepicker
+    },
+
+    data: {
+        date: new Date()
+    }
+});
